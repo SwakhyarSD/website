7eb4ea48e496e57ae9fe17b66ef7da3352092b8f
SwakhyarSD <swakhyarsauravdas@gmail.com>
2025-06-05 22:30:28 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function buyNow() {
   // Replace with your actual Gumroad product URL
-  window.location.href = "https://sauravsky6.gumroad.com/l/neegh";
+  window.location.href = "https://sauravsky6.gumroad.com/l/rxwmsk";
 }
